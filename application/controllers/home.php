a14394d44de7923c0f69510a46e4bb1fdc9aed6b
CodeIgnite <thienhoanguit@gmail.com>
2017-07-26 10:50:36 +0700

    Cập nhật Template ngày 26/07

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,14 +9,19 @@
 		{
 			//Gọi đến hàm khởi tạo của cha
 			parent::__construct();
+			//Load thư viện Url
+			$this->load->helper('url');
 		}
 		//Hàm load trang chủ
 		public function index()
 		{
+			$data = array('base_url'=> base_url());
 			//Load view trang chủ lên
-			$this->load->view('layouts/header');
-			$this->load->view('layouts/home');
-			$this->load->view('layouts/footer');
+			$this->load->view('layouts/header_View',$data);
+			$this->load->view('layouts/slider_View',$data);
+			$this->load->view('layouts/left_siderbar_View',$data);
+			$this->load->view('layouts/home_View',$data);
+			$this->load->view('layouts/footer_View',$data);
 		}
 	}
  ?>
