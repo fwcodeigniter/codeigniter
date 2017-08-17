e524ab01a8107d230dd642e986ac3b603df12fd5
khacphuong93 <khacphuong93@users.noreply.github.com>
2017-08-17 19:40:59 +0700

    image home, home_view
    chỉnh sửa nội dung và hình ảnh phù hợp với web

--- application/views/layouts/slider_View.php
+++ application/views/layouts/slider_View.php
@@ -12,39 +12,41 @@
 						<div class="carousel-inner">
 							<div class="item active">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free E-Commerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<h1><span>THẾ GIỚI CÔNG NGHỆ</span></h1>
+									<h2>Tận tâm phục vụ khách hàng</h2>
+									<p>Cửa hàng chúng tôi quyết tâm hoạt động, xây dựng phong cách phục vụ khách hàng cho tất cả các mảng kinh doanh dù mới hay cũ, lấy đó làm nền tảng tăng trưởng bền vững, hoàn thiện hình ảnh một thương hiệu gần gũi, thân thiện và hướng tới mục tiêu phục vụ khách hàng là ưu tiên hàng đầu.</p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
 								<div class="col-sm-6">
 									<img src="<?php echo $base_url; ?>/public/images/home/girl1.jpg" class="girl img-responsive" alt="" />
-									<img src="<?php echo $base_url; ?>/public/images/home/pricing.png"  class="pricing" alt="" />
+									<!--<img src="<?php echo $base_url; ?>/public/images/home/pricing.png"  class="pricing" alt="" />-->
 								</div>
 							</div>
 							<div class="item">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>100% Responsive Design</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<h1><span>THẾ GIỚI CÔNG NGHỆ</span></h1>
+									<h2>Đáp ứng mọi nhu cầu</h2>
+									<p>Thế Giới Công Nghệ chuyên về lĩnh vực điện thoại, laptop, máy tinh bảng, máy in với giá thành cạnh tranh nhất thị trường. Phù hợp với mọi người, mọi lứa tuổi, ngành nghề.</p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
 								<div class="col-sm-6">
 									<img src="<?php echo $base_url; ?>/public/images/home/girl2.jpg" class="girl img-responsive" alt="" />
-									<img src="<?php echo $base_url; ?>/public/images/home/pricing.png"  class="pricing" alt="" />
+									<!--<img src="<?php echo $base_url; ?>/public/images/home/pricing.png"  class="pricing" alt="" />-->
 								</div>
 							</div>
 							
 							<div class="item">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free Ecommerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<h1><span>THẾ GIỚI CÔNG NGHỆ</span></h1>
+									<h2>Không lo về giá</h2>
+									<p>Đừng bỏ lỡ cơ hội sở hữu những sản phẩm công nghệ tại Thế Giới Công Nghệ với chương trình khuyến mại GIẢM GIÁ.
+                                                                            Áp dụng với tất cả các sản phẩm hiện có của Thế Giới Công Nghệ các mặt hàng: Điện thoại, Laptop, Máy tính bảng, Máy in.</br>
+Áp dụng cho mua tại shop và mua online (Ship hàng toàn quốc).</p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
 								<div class="col-sm-6">
 									<img src="<?php echo $base_url; ?>/public/images/home/girl3.jpg" class="girl img-responsive" alt="" />
-									<img src="<?php echo $base_url; ?>/public/images/home/pricing.png" class="pricing" alt="" />
+									<!--<img src="<?php echo $base_url; ?>/public/images/home/pricing.png"  class="pricing" alt="" />-->
 								</div>
 							</div>
 							
