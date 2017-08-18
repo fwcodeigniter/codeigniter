e94f0c579248e78d10540d9743f1747e858025c9
khacphuong93 <khacphuong93@users.noreply.github.com>
2017-08-18 22:45:13 +0700

    bổ sung sql

--- application/views/layouts/home_View.php
+++ application/views/layouts/home_View.php
@@ -91,113 +91,42 @@
 							</div>
 							
 							<div class="tab-pane fade" id="maytinhbang" >
+								<?php if (isset($product)):foreach ($product as $key => $value):if ($value->cat_id==4){ ?>
 								<div class="col-sm-3">
 									<div class="product-image-wrapper">
 										<div class="single-products">
 											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/ipad.jpg" alt="" />
-												<h2>8.999.000đ</h2>
-												<p>iPad Wi-Fi 32GB (2017</p>
+												<img src="<?php echo $base_url; ?>/public/images/products/<?php echo $value->image?>" alt="" />
+												<h2><?php echo number_format($value->price,3); echo "đ"?></h2>
+												<p><?php echo $value->name?></p>
 												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 											</div>
 											
 										</div>
 									</div>
 								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/sstab.jpg" alt="" />
-												<h2>9.999.000đ</h2>
-												<p>Samsung Tab S2 9.7 inch</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/lenovo.jpg" alt="" />
-												<h2>3.990.000đ</h2>
-												<p>Lenovo Phab 2</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/huawei.jpg" alt="" />
-												<h2>4.290.000đ</h2>
-												<p>Huawei MediaPad T3 8.0 (2017</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
+                                                            <?php }endforeach; endif; ?>
 							</div>
 							
 							<div class="tab-pane fade" id="laptop" >
+								<?php if (isset($product)):
+                                                                foreach ($product as $key => $value): ?>
+        
+                                                            <?php if ($value->cat_id==3){ ?>
 								<div class="col-sm-3">
 									<div class="product-image-wrapper">
 										<div class="single-products">
 											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/asus.jpg" alt="" />
-												<h2>6.999.000đ</h2>
-												<p>Asus E502NA-GO010</p>
+												<img src="<?php echo $base_url; ?>/public/images/products/<?php echo $value->image?>" alt="" />
+												<h2><?php echo number_format($value->price,3); echo "đ"?></h2>
+												<p><?php echo $value->name?></p>
 												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 											</div>
 											
 										</div>
 									</div>
 								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/dell.png" alt="" />
-												<h2>10.290.000đ</h2>
-												<p>Dell Ins N3458/i3-5005U</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/macbook.jpg" alt="" />
-												<h2>33.999.000đ</h2>
-												<p>Macbook 12 256GB (2017</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="<?php echo $base_url; ?>/public/images/home/msi.jpg" alt="" />
-												<h2>20.999.000đ</h2>
-												<p>MSI GL62M 7RDX-1817XVN</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
+                                                            <?php }endforeach; endif; ?>
 							</div>
 							
 							<div class="tab-pane fade" id="poloshirt" >
@@ -262,87 +191,30 @@
 						
 						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
 							<div class="carousel-inner">
-								<div class="item active">	
-									<div class="col-sm-4">
+								
+                                                                    <?php $dem=1;if (isset($product)):foreach ($product as $key => $value):?>
+                                                                    <div class="item active">
+                                                                        <?php if ($value->sold>=3 && $dem<4){?>
+									<div class="col-sm-4">                                            
 										<div class="product-image-wrapper">
 											<div class="single-products">
+
+
 												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/s8.jpg" alt="" />
-                                                                                                        <h2>18.990.000đ</h2>
-                                                                                                        <p>Samsung Galaxy S8 Plus</p>
+													<img src="<?php echo $base_url; ?>/public/images/products/<?php echo $value->image?>" alt="" />
+                                                                                                        <h2><?php echo number_format($value->price,3); echo "đ"?></h2>
+                                                                                                        <p><?php echo $value->name?></p>
 													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
 										</div>
 									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/macbook.jpg" alt="" />
-                                                                                                        <h2>33.999.000đ</h2>
-                                                                                                        <p>Macbook 12 256GB (2017</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/mayin2.jpg" alt="" />
-                                                                                                        <h2>4.390.000đ</h2>
-                                                                                                        <p>Máy in HP GT5820 (M2Q28A)</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="item">	
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/msi.jpg" alt="" />
-                                                                                                        <h2>20.999.000đ</h2>
-                                                                                                        <p>MSI GL62M 7RDX-1817XVN</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/sstab.jpg" alt="" />
-                                                                                                        <h2>9.999.000đ</h2>
-                                                                                                        <p>Samsung Tab S2 9.7 inch</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="<?php echo $base_url; ?>/public/images/home/dell.png" alt="" />
-                                                                                                        <h2>10.290.000đ</h2>
-                                                                                                        <p>Dell Ins N3458/i3-5005U</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
+                                                                    <?php $dem++;}?>
+                                                                    </div>
+                                                                    
+                                                                    <?php endforeach; endif; ?>
+						
 								</div>
 							</div>
 							 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
